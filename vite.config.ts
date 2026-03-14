a42e5170dce86a296681243a89dd821fd1695dc7
Alexandr Plotnikov <alexandrplotnikov@Noutbuk-Alexandr.local>
2026-03-14 13:42:28 +0300

    chore: добавил алиасы в vite и ts конфиги

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@app': path.resolve(__dirname, './src/app'),
+    },
+  },
 });
